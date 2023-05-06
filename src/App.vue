14993dad5862a86ee315bb6143d228f1c8310674
Косенко Олексій <jgjghhjgjghh1@gmail.com>
2023-05-06 21:28:50 +0300

    add Action GET USERS

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
+<script>
+import { mapActions, mapGetters } from 'vuex';
+// import { RouterLink, RouterView } from 'vue-router'
 
+export default {
+  computed: {
+    ...mapGetters([
+      'USERS'
+    ])
+  },
+
+  methods: {
+    ...mapActions([
+      'GET_USER_FROM_API'
+    ])
+  },
+  mounted() {
+    this.GET_USER_FROM_API()
+},
+}
 </script>
 
 <template>
-
+  
 </template>
 
 <style scoped>
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -7,8 +7,21 @@ export const store = createStore({
         users: [],
     },
     actions: {
-
+        GET_USER_FROM_API({commit}) {
+            return axios.get('http://localhost:3000/users') 
+            .then((response) => {
+                commit('SET_USERS_TO_COMMIT', response.data)
+            })
+        }
     },
-    mutations: {},
-    getters: {}
+    mutations: {
+        SET_USERS_TO_COMMIT(state, users) {
+            state.users = users
+        }
+    },
+    getters: {
+        USERS(state) {
+            return state.users
+        }
+    }
 })
