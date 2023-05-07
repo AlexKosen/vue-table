f4b51f8fe281ec5d6ea112e2bbb23bb9ad35c588
Косенко Олексій <jgjghhjgjghh1@gmail.com>
2023-05-07 14:46:12 +0300

    create paginations tab

--- src/components/table/v-table.vue
+++ src/components/table/v-table.vue
@@ -4,6 +4,25 @@ import vTableRow from '../v-table-row.vue';
 export default {
     components: {
         vTableRow,
+
+    },
+
+    data() {
+        return {
+            usersPerPage: 10, 
+            pageNumber: 1,           
+        }
+    },
+
+    computed: {
+        pages () {
+            return Math.ceil(this.users_data.length / this.usersPerPage)
+        },
+        paginationUsers() {
+            let from = (this.pageNumber - 1) * this.usersPerPage
+            let to = from + this.usersPerPage
+            return this.users_data.slice(from, to)
+        },
     },
 
     props: {
@@ -12,6 +31,11 @@ export default {
             default: () => {
                 return []
             }
+        }
+    },
+    methods: {
+        pageClick(page) {
+            this.pageNumber = page
         }
     }
 }
@@ -29,11 +53,19 @@ export default {
         <div class="v-table__body">
             <div class="body__row">
                 <v-table-row
-                    v-for="row in users_data" 
+                    v-for="row in paginationUsers" 
                     :key="row.id"
                     :row_data = "row"
                 />                
             </div>
+        </div>
+        <div class="v-table__pagination">
+            <div class="page"
+                v-for="page in pages"
+                :key="page"
+                :class="{'page__selected': page === pageNumber}"
+                @click="pageClick(page)"
+            >{{ page }}</div>
         </div>
     </div>
   
@@ -47,5 +79,31 @@ export default {
 .v-table__header {
     display: flex;
     justify-content: space-around;
+    border-bottom: solid 1px #e7e7e7;
+}
+.v-table__header p {
+    flex-basis: 25%;
+    text-align: left;
+}
+.v-table__pagination {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 30px;
+}
+.page {
+    padding: 8px;
+    margin-right: 10px;
+    border: solid 1px #e7e7e7
+}
+.page:hover {
+    cursor: pointer;
+    background-color: #aeaeae;
+    color: #fff;
+}
+.page__selected {
+    cursor: pointer;
+    background-color: #aeaeae;
+    color: #fff;
 }
 </style>
--- src/components/v-table-row.vue
+++ src/components/v-table-row.vue
@@ -13,10 +13,10 @@ export default {
 
 <template>
     <div class="v-table-row">
-        <div class="row__name">{{ row_data.name }}</div>
-        <div class="row__earned">{{ row_data.points_earned }}</div>
-        <div class="row__spent">{{ row_data.points_spent }}</div>
-        <div class="row__date">{{ row_data.registration_date }}</div>
+        <div class="row row__name">{{ row_data.name }}</div>
+        <div class="row row__earned">{{ row_data.points_earned }}</div>
+        <div class="row row__spent">{{ row_data.points_spent }}</div>
+        <div class="row row__date">{{ row_data.registration_date }}</div>
     </div>
 </template>
 
@@ -24,5 +24,10 @@ export default {
     .v-table-row {
         display: flex;
         justify-content: space-around;
+        padding: 10px 0;
+    }
+    .row {
+        flex-basis: 25%;
+        text-align: left;
     }
 </style>
